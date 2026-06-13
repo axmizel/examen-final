adc94b031deb08c51c261587e087034c28a182c0
axmizel <manuel.rubilar.sepulveda@estudiante.ipss.cl>
2026-06-13 19:43:59 -0400

    update project

--- src/components/molecules/CardInfo.jsx
+++ src/components/molecules/CardInfo.jsx
@@ -1,12 +1,12 @@
-function CardInfo({ titulo = "Sin título", descripcion = "Sin descripción" }) {
+function CardInfo({ title, description }) {
   return (
     <div className="card shadow h-100">
       <div className="card-body">
-        <h5 className="card-title">{titulo}</h5>
-        <p className="card-text">{descripcion}</p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
       </div>
     </div>
   );
 }
- 
+
 export default CardInfo;
